c38bb8f7a162da0df0c4de1242484846d282f882
asalinas9 <salinas99@live.com>
2020-05-21 17:04:47 -0500

    corrected bug and changed game theme

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,92 +1,124 @@
-$(document).ready(function() {
-	var ranNum = Math.floor(Math.random() * 102 + 19);
-	$('.matchedNumber').text(ranNum);
-
-	var box1 = Math.floor(Math.random() * 12 + 1);
-	var box2 = Math.floor(Math.random() * 12 + 1);
-	var box3 = Math.floor(Math.random() * 12 + 1);
-	var box4 = Math.floor(Math.random() * 12 + 1);
-
-	//counter
-	var wins = 0;
-	var losses = 0;
-	var playerScore = 0;
-
-	$('#numberWins').text(wins);
-	$('#numberLosses').text(losses);
-	reset();
-});
-//functions
-function reset() {
-	ranNum = Math.floor(Math.random() * 102 + 19);
-	console.log(ranNum);
-	$('.matchedNumber').text(ranNum);
-
-	box1 = Math.floor(Math.random() * 12 + 1);
-	box2 = Math.floor(Math.random() * 12 + 1);
-	box3 = Math.floor(Math.random() * 12 + 1);
-	box4 = Math.floor(Math.random() * 12 + 1);
-	playerScore = 0;
-	$('#finalScore').text(playerScore);
+let $ = function (id) {
+	return document.getElementById(id)
 }
-// wins towards finalScore
-function beast() {
-	alert('You Win');
-	wins++;
-	$('#numberWins').text(wins);
-	reset();
+
+//global variable
+const words = ["goku", "vegeta", "trunks", "jiren", "hit", "android17", "masterroshi"];
+const images = ["assets/images/DB1.png", "assets/images/DB2.png", "assets/images/DB3.png", "assets/images/DB4.png", "assets/images/DB6.png", "assets/images/DB7.png", "assets/images/DB8.png"];
+let word;
+const img = document.createElement("img")
+const parent = $("img")
+let answerArray = [];
+let userGuess;
+let rightGuess = false;
+let userRightGuess = 0;
+let left = 9;
+let wins = 0;
+let losses = 0;
+
+
+//random word
+function random() {
+	let random = Math.floor(Math.random() * words.length);
+	word = words[random]
+	img.src = images[random]
 }
-//losses of playerScore
-function loser() {
-	alert('You lose');
-	losses++;
-	$('#numberLosses').text(losses);
-	reset();
+
+//show blank start
+function showBlank() {
+	for (i = 0; i < word.length; i++) {
+		answerArray[i] = "_"
+	}
+	$("guess").innerHTML = answerArray.join(" ")
 }
-// on click for logos
-$('.barca').on('click', function() {
-	playerScore = playerScore + box1;
-	console.log('New playerScore= ' + playerScore);
-	$('#finalScore').text(playerScore);
 
-	if (playerScore == ranNum) {
-		beast();
-	} else if (playerScore > ranNum) {
-		loser();
+//guesses left
+function guessesLeft() {
+	$("left").innerHTML = left
+}
+
+//wins
+function winsScore() {
+	$("wins").innerHTML = wins
+}
+
+//losses
+function lossesScore() {
+	$("losses").innerHTML = losses
+}
+
+//show wrong guess
+function wrongGuess(char) {
+	$("wrong").innerHTML += char + ", "
+}
+
+// resent function
+function initialGame() {
+	if ($("winImage")) {
+		$("winImage").remove()
 	}
-});
 
-$('.liverpool').on('click', function() {
-	playerScore = playerScore + box2;
-	console.log('New playerScore= ' + playerScore);
-	$('#finalScore').text(playerScore);
+	left = 9;
+	answerArray = [];
+	$("wrong").innerHTML = "";
+	userRightGuess = 0
+	rightGuess = false;
+	guessesLeft()
+	random()
+	showBlank()
+}
 
-	if (playerScore == ranNum) {
-		beast();
-	} else if (playerScore > ranNum) {
-		loser();
+// call initial function
+initialGame()
+winsScore()
+lossesScore()
+
+//check letter
+function showLetter(char, str) {
+	for (let j = 0; j < str.length; j++) {
+		if (char === str[j]) {
+			rightGuess = true
+			answerArray.splice(j, 1, char)
+			userRightGuess++
+		}
 	}
-});
-$('.juve').on('click', function() {
-	playerScore = playerScore + box3;
-	console.log('New playerScore= ' + playerScore);
-	$('#finalScore').text(playerScore);
+	$("guess").innerHTML = answerArray.join(" ")
+}
 
-	if (playerScore == ranNum) {
-		beast();
-	} else if (playerScore > ranNum) {
-		loser();
+//check length
+let matchLength = function () {
+	if (word.length === userRightGuess) return true
+	else return false
+}
+
+//user guess
+document.onkeyup = function (event) {
+	userGuess = event.key.toLowerCase();
+
+	showLetter(userGuess, word)
+
+	if (rightGuess) {
+		rightGuess = false
+		if (matchLength()) {
+
+			img.setAttribute("id", "winImage")
+			parent.appendChild(img)
+			wins++
+			winsScore()
+			setTimeout(initialGame, 3000)
+		}
+	} else {
+		left--
+		if (left < 1) {
+
+			initialGame()
+			losses++
+			lossesScore()
+		} else {
+
+			wrongGuess(userGuess)
+			guessesLeft()
+		}
+
 	}
-});
-
-$('.realM').on('click', function() {
-	playerScore = playerScore + box4;
-	console.log('New playerScore= ' + playerScore);
-	$('#finalScore').text(playerScore);
-
-	if (playerScore == ranNum) {
-		beast();
-	} else if (playerScore > ranNum) {
-		loser();
-	}
-});
+}
